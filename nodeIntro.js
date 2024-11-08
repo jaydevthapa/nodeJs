09557ec94e350b839812547ee0199a6aae99c027
Jaydev Thapa <thapajaydev6@gmail.com>
2024-11-08 19:24:18 +0545

    a short introduction to nodejs

---
+++ nodeIntro.js
@@ -0,0 +1,25 @@
+/* 
+what is node js?
+=>Nodejs is nothing but the run time enviroment for the javascript.
+  Ryan Dahl emmbeded V8 engine (a chrome engine to run js) with c++ which he named the project as nodejs
+  the benifit of nodejs is we can run js outside the browser and can talk to native machine because of c++
+  and last but not least we can create webservers in js language.
+
+*/
+
+
+/*
+Installation of nodejs
+steps
+1. go to the website "nodejs.org"
+2. scroll down and you will see two differents button to downloads as Lts and Current
+3. Lts means long term support which is stable relese and current is beta version to test new version.
+4. it is odd even approch even numbers are lts version and odd is current version
+5. always install even version 
+
+check wether the node install or not 
+1. open terminal 
+2. write command "node --version"
+3. npm(node package manager) is by defult install with nodejs to check the version you can write the command "npm -v";
+4. if all goes right we are good to go.
+*/
